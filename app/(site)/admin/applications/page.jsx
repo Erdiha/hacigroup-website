770357c00bedfebcec40789bd1a2e6e6dcdeba30
erdiha <erdiha@gmail.com>
2025-11-21 07:50:04 -0800

    fireabse bug fix

--- app/(site)/admin/applications/page.jsx
+++ app/(site)/admin/applications/page.jsx
@@ -2,6 +2,9 @@
 
 import AdminDashboard from "../AdminDashboard";
 
+// Force dynamic rendering - don't attempt static generation
+export const dynamic = 'force-dynamic';
+
 export default function AdminApplicationsPage() {
   return <AdminDashboard initialSection="applications" />;
 }
